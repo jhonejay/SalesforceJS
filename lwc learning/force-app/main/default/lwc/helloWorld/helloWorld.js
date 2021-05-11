f2f8607f0c3acecd5caa8cf3240f36c48d676dca
DEV-Jhonejay <79820735+dev-jhonejay@users.noreply.github.com>
2021-05-11 10:05:09 +0800

    05112021

--- lwc learning/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ lwc learning/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,4 +1,24 @@
 import { LightningElement } from 'lwc';
-//import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement { }
+// export default class HelloWorld extends LightningElement {
+//     name ///undefined
+//     age = 30;
+//     fullname = "Salesforce india";
+//     showData = false;
+//     details = {
+//         name: "dummy",
+//         place: "Melbourne"
+//     }
+//     userList = ["A", "B", "C"]
+// }
+
+
+export default class HelloWorld extends LightningElement {
+    fullname = "Zero To Hero";
+    title = "aura";
+
+    changeHandler(event) {
+        this.title = event.target.value;
+    }
+
+}
