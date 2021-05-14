0c8a450d2be4d8cd1191963b4b405bfa91090cfc
DEV-Jhonejay <79820735+dev-jhonejay@users.noreply.github.com>
2021-05-14 10:35:14 +0800

    salesforce 05142021

--- lwc learning/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ lwc learning/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 // export default class HelloWorld extends LightningElement {
 //     name ///undefined
@@ -14,11 +14,46 @@ import { LightningElement } from 'lwc';
 
 
 export default class HelloWorld extends LightningElement {
+
+    //data binding example
     fullname = "Zero To Hero";
     title = "aura";
 
     changeHandler(event) {
         this.title = event.target.value;
     }
+    //end data binding
 
+    //@track properties
+    @track address = {
+        city: "Melbourne",
+        postcode: 3808,
+        country: "Australia"
+    }
+    userList = ["a", "b", "c"]
+
+    trackHandler(event) {
+        this.address.city = event.target.value;
+        //this.address = { ... this.address, "city": event.target.value }
+        //this.userList[0] = "test"
+    }
+    //end @track properties
+
+    //getters example
+    users = ["john", "smith", "nik"]
+    num1 = 10
+    num2 = 20
+
+    get firstUser() {
+        return this.users[0].toUpperCase();
+    }
+
+    get total() {
+        return this.num1 * this.num2;
+    }
+    //end getters example
+
+    //conditional Rendering
+
+    //end conditional Rendering
 }
